6e34302b7d9f5af506d17de318fc8e9b6b8a7bf0
Adnan Ali <adnanali6688@gmail.com>
2022-06-02 22:13:40 +0200

    created cart component

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import MealsSummary from './MealsSummary'
 import AvailableMeals from "./AvailableMeals";
+import Cart from "../Cart/Cart";
 
 const Meals = () => {
     return <Fragment>
         <MealsSummary />
         <AvailableMeals />
+        <Cart />
     </Fragment>
 }
 

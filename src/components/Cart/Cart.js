7de1519f910f9ed5f81d954fc34f4da1485e7d17
Adnan Ali <adnanali6688@gmail.com>
2022-06-27 20:33:21 +0200

    implemented messages to show during http request processing

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,6 +8,8 @@ import Checkout from './Checkout'
 const Cart = (props) => {
 
     const [isForm, setIsForm] = useState(false)
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const [didSubmit, setDidSubmit] = useState(false)
 
     const cartCtx = useContext(CartContext)
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -44,25 +46,42 @@ const Cart = (props) => {
         {hasItems && <button className={styles.button} onClick={orderHandler}>Order</button>}
     </div>
 
-    const orderConfirmHandler = (userData) => {
-        fetch('https://meals-project-f49bf-default-rtdb.europe-west1.firebasedatabase.app/orders.json', {
+    const orderConfirmHandler = async (userData) => {
+        setIsSubmitting(true)
+        await fetch('https://meals-project-f49bf-default-rtdb.europe-west1.firebasedatabase.app/orders.json', {
             method: 'POST',
             body: JSON.stringify({
                 user: userData,
                 order: cartCtx.items
             })
         })
+        setIsSubmitting(false)
+        setDidSubmit(true)
     }
+
+    const modal = <>
+        {cartItems}
+        <div className={styles.total}>
+            <span>Total Amount</span>
+            <span>{totalAmount}</span>
+        </div>
+        {isForm && <Checkout onConfirm={orderConfirmHandler} onCancel={props.onClose}/>}
+        {!isForm &&  modalActions}
+    </>
+
+    const isSubmittingContent = <p>Your order is submitting...</p>
+    const didSubmitContent = <>
+        <p>Your order is submitted successfully</p>
+        <div className={styles.actions}>
+            <button className={styles.actions} onClick={props.onClose}>Close</button>
+        </div>
+    </>
 
     return (
         <Modal onClose={props.onClose}>
-            {cartItems}
-            <div className={styles.total}>
-                <span>Total Amount</span>
-                <span>{totalAmount}</span>
-            </div>
-            {isForm && <Checkout onConfirm={orderConfirmHandler} onCancel={props.onClose}/>}
-            {!isForm &&  modalActions}
+            {!isSubmitting && !didSubmit && modal}
+            {isSubmitting && isSubmittingContent}
+            {!isSubmitting && didSubmit && didSubmitContent}
         </Modal>
     )
 }
